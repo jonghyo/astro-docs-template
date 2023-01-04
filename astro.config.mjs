58b96ec20f4492fc6b38d4ddf8dd8f19689c7ace
jonghyo <m.73816686@gmail.com>
2023-01-04 17:47:18 +0900

    Update astro.config.mjs
    Signed-off-by: jonghyo <m.73816686@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:3000/",
-  base: "/mbaas/mbaas-docs",
-  integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-    tailwind(),
-  ],
+	site: 'https://palette.pages.devops.aslead.cloud/',
+	base: process.env.BASE_URL || '/foo/',
+	integrations: [
+		// Enable Preact to support Preact JSX components.
+		preact(),
+		// Enable React for the Algolia search component.
+		react(),
+		tailwind(),
+		mdx(),
+	],
 });
